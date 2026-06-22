5662ca2dcad8a06228d6862e8865064922620d67
Naila Tabassum <subhajarin25@gmail.com>
2026-06-22 20:25:10 +0600

    Create practice.py
    Only practices

---
+++ practice.py
@@ -0,0 +1,34 @@
+#normal prints
+print('student name')
+print('student id')
+print('course name')
+#variable included
+a='Student Name:'
+b='Student Id:'
+c='Course Name:'
+print(a)
+print(b)
+print(c)
+#using one print for three different value
+a="Naila"
+b="2630479"
+c="Cse100"
+print(a+'\n'+b+'\n'+c)
+#print the type of the variables
+a='Naila'
+b=2630479
+c=4.00
+d=True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+#print the word length
+a='Hello, This is me Naila Rubu,Who is currently studying in Computer Science and Engeneering at IUB.'
+print(a)
+print(len(a))
+#using a single assignment operator
+a,b,c=7,8.41,'Jonny Jonny Yes Papa'
+print(a)
+print(b)
+print(c)
